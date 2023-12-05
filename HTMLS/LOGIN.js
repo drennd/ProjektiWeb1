9ab7a91a3630fb0fbad2072db98ec48e2301b452
drennd <dm64750@ubt-uni.net>
2023-12-05 17:38:45 +0100

    e rregullova login form qe me na dergu drejt ne main

--- HTMLS/LOGIN.js
+++ HTMLS/LOGIN.js
@@ -1,18 +1,20 @@
 function validateForm() {
     var user = document.loginForm.usr.value;
     var pass = document.loginForm.pwd.value;
-    var username = "username";
+    var username = "Username";
     var password = "password";
     
-    if ((user == username) && (pass == password)) {
-        // Redirect to Home.html when login is successful
+
+
+    if ((user != '') && (pass != '')) {
+        
         window.location.href = 'Home.html';
         return true;
     } else {
-        // Display alert before redirecting to LOGIN.html
+         
         alert("Login was unsuccessful, please check your username and password");
         
-        // Redirect to LOGIN.html when login is unsuccessful
+        
         window.location.href ='LOGIN.html';
         return false;
     }
